0574472e0e22afb5e1c39d4b8995da4d3a909ca3
DiarraIbra <ibrahimadiarraibou@gmail.com>
2025-07-09 11:41:56 +0100

    Rendu de liste

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,19 @@ const image =ref(socksGreen)
 const url_link = 'https://vuejs.org/'
 const inStock = ref(true)
 const onSale = ref(false)
+const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const variants=ref([
+  {
+    id: 2234,
+    color: 'green',
+    dispo:false
+  },
+  {
+    id: 2235,
+    color: 'blue',
+    dispo:true
+  }
+])
 const updateCart = (id) => {
   cart.value.push(id)
 }
@@ -21,6 +34,16 @@ const updateCart = (id) => {
     <p v-else>En rupture de stock</p>
     <p v-if="onSale">Actuellement en solde.</p>
     <p v-else>Pas de solde disponible actuellement.</p>
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+    Voici la liste des couleurs disponibles sont :
+    <ul>
+      <li  v-for="variant in variants" :key="variant.id">
+        <p v-if="variant.dispo">
+         variant dispo  {{ variant.color }}
+        </p></li>
+    </ul>
   </div>
   <div class="nav-bar"></div>
   <div class="cart">Cart({{ cart.length }})</div>
