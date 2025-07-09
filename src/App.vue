71f94bfbef9b653f67653cbcf38e59aada7c7bb5
DiarraIbra <ibrahimadiarraibou@gmail.com>
2025-07-09 12:41:44 +0100

    Liaison de classes et de styles

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,12 @@ const addToCartTest=()=> {
   cart_test.value+=1
   console.log(cart_test.value)
 }
+const decremeter=()=>{
+  if(cart_test.value <= 0) {
+    return alert('Le panier est vide')
+  }
+  cart_test.value-=1
+}
 </script>
   
 <template>
@@ -52,8 +58,24 @@ const addToCartTest=()=> {
     </ul>
     La couleur du drapeau du mali est : <div v-for="tab in tableau">{{ tab }}</div>
     <button @click="addToCartTest">Ajouter au panier</button>
+    <button @click="decremeter">Enlever du panier</button>
+    <span class="span-button">{{cart_test}}</span>
   </div>
   <div class="nav-bar"></div>
   <div class="cart">Cart({{ cart.length }})</div>
   <ProductDisplay :premium="premium" @add-to-cart="updateCart"></ProductDisplay>
 </template>
+<style>
+.span-button{
+  background-color: red;
+  color: white;
+  padding: 5px;
+  margin-left: 20px;
+  border-radius: 2px;
+  cursor: pointer;
+  width: 20%;
+  display: block;
+  margin: 20px auto;
+  text-align: center;
+}
+</style>
