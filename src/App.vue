25043c585eff46ef37990bffb1dab09d193e61d3
DiarraIbra <ibrahimadiarraibou@gmail.com>
2025-07-09 10:18:39 +0100

    creation de l'applicatoon vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import ProductDisplay from '@/components/ProductDisplay.vue'
+const desc=ref("je suis une description")
+const cart = ref([])
+const premium = ref(true)
+
+const updateCart = (id) => {
+  cart.value.push(id)
+}
+</script>
+  
+<template>
+  <p>Ma description est <strong>{{ desc }}</strong></p>
+  <div class="nav-bar"></div>
+  <div class="cart">Cart({{ cart.length }})</div>
+  <ProductDisplay :premium="premium" @add-to-cart="updateCart"></ProductDisplay>
+</template>
